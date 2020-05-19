e8d568bac3ae113c5121bc0a81612427f78df68b
charles161 <fortloop.charles@gmail.com>
2020-05-20 00:38:05 +0530

    Got pixel color for each pixel

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Jimp = require("jimp");
+const math = require("mathjs");
 
 const l = console.log;
 
@@ -8,17 +9,29 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
+function getPixelColors(image) {
+  let width = image.bitmap.width;
+  let height = image.bitmap.height;
+  let count = 0;
+  let ans = [[]];
+  for (let i = 0; i < width; i++) {
+    for (let j = 0; j < height; j++) {
+      let rgbObj = Jimp.intToRGBA(image.getPixelColor(i, j));
+      ans[count] = [rgbObj.r / 255, rgbObj.g / 255, rgbObj.b / 255];
+      count++;
+    }
+  }
+  return math.matrix(ans);
+}
+
 Jimp.read("insta.png")
   .then(image => {
-    let width = image.bitmap.width;
-    let height = image.bitmap.height;
-    for (let i = 0; i < width; i++)
-      for (let j = 0; j < height; j++) {
-        let rgbObj = Jimp.intToRGBA(image.getPixelColor(i, j));
-        const hex = Jimp.rgbaToInt(getRandomInt(145, 150), getRandomInt(145, 150), getRandomInt(145, 150), rgbObj.a);
-        image.setPixelColor(hex, i, j);
-      }
-    image.write("insta.png");
+    let X = getPixelColors(image);
+    let count = 0;
+    X.forEach(function(value, index, matrix) {
+      if (count <= 20) console.log("value:", value, "index:", index);
+      count++;
+    });
   })
   .catch(err => {
     l(err);
